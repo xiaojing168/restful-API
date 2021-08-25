ddb899d87cfd8bb54b344fb402728e8131a42a80
xiaojing168 <77518150+xiaojing168@users.noreply.github.com>
2021-08-25 15:03:44 -0400

    Add files via upload

---
+++ app.js
@@ -0,0 +1,104 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const ejs = require('ejs');
+const mongoose = require('mongoose');
+
+const app = express();
+app.set('view engin',ejs);
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(express.static('public'));
+
+mongoose.connect('mongodb://localhost:27017/wikiDB',{useNewUrlParser: true, useUnifiedTopology: true});
+
+const articleSchema = {
+  title: String,
+  content: String
+};
+const Article = mongoose.model('Article', articleSchema);
+///////////////Request Targetting All the Articles///////////////////////
+app.route('/articles')
+.get(function(req,res){
+  Article.find(function(err,foundArticles){
+    if(!err){
+      res.send(foundArticles);
+    }else{
+      res.send(err);
+    }
+
+})
+})
+
+.post(function(req,res){
+const newArticle = new Article({
+  title: req.body.title,
+  content: req.body.content
+});
+newArticle.save(function(err){
+  if(!err){
+    res.send('Successfully added a new article.')
+  }else{
+    res.send(err);
+  }
+});
+})
+
+.delete(function(req,res){
+  Article.deleteMany(function(err){
+    if(!err){
+      res.send('Successfully deleted all articles.');
+    }else{
+      res.send(err);
+    }
+  });
+});
+///////////////Request Targetting A Specific Article///////////////////////
+app.route('/articles/:articleTitle')
+
+.get(function(req,res){
+
+
+  Article.findOne({title: req.params.articleTitle},function(err,foundArticle){
+    if(foundArticle){
+      res.send(foundArticle);
+    }else{
+      res.send(err);
+    }
+  })
+})
+.put(function(req,res){
+  Article.update(
+    {title: req.params.articleTitle},
+  {title: req.body.title, content: req.body.content},
+  {overwrite:true},
+  function(err){
+    if(!err){
+      res.send('Successfully update article');
+    }
+  })
+})
+.patch(function(req,res){
+  Article.update({title: req.params.articleTitle},
+  {$set:req.body},
+function(err){
+  if(!err){
+    res.send('Successfully update article');
+  }else{
+    res.send(err);
+  }
+});
+})
+.delete(function(req,res){
+  Article.deleteOne({title: req.params.articleTitle},function(err){
+    if(!err){
+      res.send('Successfully');
+    }else{
+      res.send(err);
+    }
+  });
+});
+
+
+
+app.listen(3000,function(){
+  console.log('Server started on port 3000');
+});
